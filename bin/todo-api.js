ffd7eb543b2023b9fefdc1925fb6660f493dbeab
Matt Rubin <mrubin888@gmail.com>
2015-11-12 06:09:26 -0800

    CORS fix

--- bin/todo-api.js
+++ bin/todo-api.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, content-type, Accept");
+	res.header("Access-Control-Allow-Methods", "GET, HEAD, PUT, DELETE");
 	next();
 });
 app.use(todoRoutes);
